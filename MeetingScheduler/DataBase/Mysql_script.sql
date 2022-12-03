6a592377fc1bfd4b4a7b057aaf948eae87e38c4b
LucasKalil-Programador <82661706+LucasKalil-Programador@users.noreply.github.com>
2022-12-02 21:36:38 -0300

    atualizado o codigo sql

--- MeetingScheduler/DataBase/Mysql_script.sql
+++ MeetingScheduler/DataBase/Mysql_script.sql
@@ -5,19 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema MeetingScheduler_DB
+-- Schema meetingscheduler_db
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `meetingscheduler_db` DEFAULT CHARACTER SET utf8 ;
+USE `meetingscheduler_db` ;
 
 -- -----------------------------------------------------
--- Schema MeetingScheduler_DB
+-- Table `meetingscheduler_db`.`Client`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `MeetingScheduler_DB` DEFAULT CHARACTER SET utf8 ;
-USE `MeetingScheduler_DB` ;
-
--- -----------------------------------------------------
--- Table `MeetingScheduler_DB`.`Client`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Client` (
+CREATE TABLE IF NOT EXISTS `meetingscheduler_db`.`Client` (
   `idClient` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(256) NOT NULL,
   `password` VARCHAR(64) NOT NULL,
@@ -30,9 +26,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MeetingScheduler_DB`.`Location`
+-- Table `meetingscheduler_db`.`Location`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Location` (
+CREATE TABLE IF NOT EXISTS `meetingscheduler_db`.`Location` (
   `idLocation` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(128) NOT NULL,
   `address` VARCHAR(256) NOT NULL,
@@ -44,9 +40,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MeetingScheduler_DB`.`Appointment`
+-- Table `meetingscheduler_db`.`Appointment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Appointment` (
+CREATE TABLE IF NOT EXISTS `meetingscheduler_db`.`Appointment` (
   `idAppointment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Client` INT UNSIGNED NOT NULL,
   `start_date_time` DATETIME NOT NULL,
@@ -56,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Appointment` (
   INDEX `fk_Appointment_Client_idx` (`Client` ASC) VISIBLE,
   CONSTRAINT `fk_Appointment_Client`
     FOREIGN KEY (`Client`)
-    REFERENCES `MeetingScheduler_DB`.`Client` (`idClient`)
+    REFERENCES `meetingscheduler_db`.`Client` (`idClient`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MeetingScheduler_DB`.`Meeting`
+-- Table `meetingscheduler_db`.`Meeting`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Meeting` (
+CREATE TABLE IF NOT EXISTS `meetingscheduler_db`.`Meeting` (
   `idMeeting` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `start_date_time` DATETIME NOT NULL,
   `end_date_time` DATETIME NOT NULL,
@@ -73,21 +69,21 @@ CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Meeting` (
   `description` VARCHAR(512) NOT NULL,
   `subject` VARCHAR(128) NOT NULL,
   `name` VARCHAR(64) NOT NULL,
-  `priority` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idMeeting`, `Location`),
+  `priority` INT NOT NULL,
+  PRIMARY KEY (`idMeeting`),
   INDEX `fk_Meeting_Location1_idx` (`Location` ASC) VISIBLE,
   CONSTRAINT `fk_Meeting_Location1`
     FOREIGN KEY (`Location`)
-    REFERENCES `MeetingScheduler_DB`.`Location` (`idLocation`)
+    REFERENCES `meetingscheduler_db`.`Location` (`idLocation`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MeetingScheduler_DB`.`Client_has_Meeting`
+-- Table `meetingscheduler_db`.`Client_has_Meeting`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Client_has_Meeting` (
+CREATE TABLE IF NOT EXISTS `meetingscheduler_db`.`Client_has_Meeting` (
   `Client` INT UNSIGNED NOT NULL,
   `Meeting` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`Client`, `Meeting`),
@@ -95,12 +91,12 @@ CREATE TABLE IF NOT EXISTS `MeetingScheduler_DB`.`Client_has_Meeting` (
   INDEX `fk_Client_has_Meeting_Client1_idx` (`Client` ASC) VISIBLE,
   CONSTRAINT `fk_Client_has_Meeting_Client1`
     FOREIGN KEY (`Client`)
-    REFERENCES `MeetingScheduler_DB`.`Client` (`idClient`)
+    REFERENCES `meetingscheduler_db`.`Client` (`idClient`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Client_has_Meeting_Meeting1`
     FOREIGN KEY (`Meeting`)
-    REFERENCES `MeetingScheduler_DB`.`Meeting` (`idMeeting`)
+    REFERENCES `meetingscheduler_db`.`Meeting` (`idMeeting`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
